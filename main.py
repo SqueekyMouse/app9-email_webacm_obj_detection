9e9a7cf5502c77bc19d288b6c3e79220d9f93603
sreejith <squeeky.mouse@gmail.com>
2023-03-02 20:04:05 +0530

    frame comaprision, get delta fame Sec36

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2
 import time
-# commit: opencv continous image capture, greyscale Sec36
+# commit: frame comaprision, get delta fame Sec36
 
 video=cv2.VideoCapture(0)
 time.sleep(1) # give cam time to load
+
+# get first frame and compare rest against the first
+first_frame=None
 
 while True:
     check,frame=video.read() # captures an image
@@ -11,7 +14,14 @@ while True:
     gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) # algo to cnv image
     # blur the image to remove the noise, dont ned that much precision
     gray_frame_gau=cv2.GaussianBlur(gray_frame,(21,21),0)
-    cv2.imshow('my video',gray_frame_gau) 
+    #cv2.imshow('my video',gray_frame_gau) #show grey blur img
+
+    # get first frame and compare rest against the first
+    if first_frame is None:
+        first_frame=gray_frame_gau
+
+    delta_frame=cv2.absdiff(first_frame,gray_frame_gau) #get the difference matrix
+    cv2.imshow('my video',delta_frame) #show the delta frame
 
     key=cv2.waitKey(1) # basically creates a keyboard key obj
     if key==ord('q'): # quit if key q is pressed!!!
