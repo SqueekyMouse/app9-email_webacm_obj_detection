933c7c011005f8c6d58b93704f31eb99ff53a03f
sreejith <squeeky.mouse@gmail.com>
2023-03-03 19:48:05 +0530

    list of obj frames and save only middle one Sec37

--- emailing.py
+++ emailing.py
@@ -1,5 +1,11 @@
-
+import os
 # commit: email section Sec36
+
+host='smtp.gmail.com'
+port=465
+username='appuser565@gmail.com'
+password=os.getenv('APP_M_PASSWORD')
+receiver='appuser565@gmail.com'
 
 def send_email():
     print('Email was sent')
--- main_f.py
+++ main_f.py
@@ -2,7 +2,7 @@ import cv2
 import time
 from emailing import send_email
 import glob
-# commit: get list of obj frames and save only middle one Sec37
+# commit: got image of frame with obj Sec37
 
 video=cv2.VideoCapture(0)
 time.sleep(1) # give cam time to load
@@ -10,7 +10,6 @@ time.sleep(1) # give cam time to load
 first_frame=None # get first frame and compare rest against the first
 status_list=[] # to track obj detection status
 count=1
-cap_img=[]
 
 while True:
     status=0 # to track obj moving out of frame
@@ -44,16 +43,18 @@ while True:
 
         if rectangle.any(): # obj detected, update status
             status=1
-            # add frames with objects to the list so we can later get the middle one to email
-            cap_img.append(frame)
+            #save frame with objects as image
+            cv2.imwrite(filename=f'images/{count}.png',img=frame) 
+            count+=1
+            # get the middle img from the set of images saved
+            all_images=glob.glob('images/*.png')
+            idx=int(len(all_images)/2)-1 # get middle index
+            image_with_obj=all_images[idx] # got img with obj to email
 
     status_list.append(status) # list of obj detection statuses
     status_list=status_list[-2:] # status 1 to 0 means obj exited frame
 
     if status_list[0]==1 and status_list[1]==0: #ie, ob exited frame so send mail
-        idx=int(len(cap_img)/2)-1 # get mid index
-        cv2.imwrite(filename='images/q.png',img=cap_img[idx]) # save the middle frame from list to file
-        cap_img=[] # clear the list
         send_email()
 
     print(status_list)
